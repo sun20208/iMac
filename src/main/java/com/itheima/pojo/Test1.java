f1d8ba137bc078556f726b0024327e6a9009a61e
sun <sun_2020_8@163.com>
2020-11-04 17:15:45 +0800

    小李提交1

--- src/main/java/com/itheima/pojo/Test1.java
+++ src/main/java/com/itheima/pojo/Test1.java
@@ -2,6 +2,8 @@ package com.itheima.pojo;
 
 public class Test1 {
     private String name;
-    private String age;
-    private String xiaoli;
+    private String ae;
+    private String xili;
+    private String xiaooli;
+    private String geyxiaowang;
 }
